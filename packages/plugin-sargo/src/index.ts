b81e41520662d9a63032867035a6118b74876e70
anthony <kimaniantoni@gmail.com>
2025-07-14 04:47:20 +0300

    chore: removes first swap functionality

--- packages/plugin-sargo/src/index.ts
+++ packages/plugin-sargo/src/index.ts
@@ -1,16 +1,14 @@
 import { Plugin, IAgentRuntime } from "@elizaos/core";
 import { getBalanceAction } from "./actions/getBalanceAction";
 import { AddressProvider } from "./providers";
-import { rewardFirstSwapAction } from "./actions/rewardFirstSwapAction";
-import { SwapListenerService } from "./services/swapListenerService";
 import { getUserDataAction } from "./actions/getUserDataAction";
 
 
 export const sargoInternPlugin: Plugin = {
     name: "sargoInternPlugin",
     description: "A plugin for interacting with sargoIntern Character",
-    actions: [getBalanceAction, rewardFirstSwapAction, getUserDataAction],
+    actions: [getBalanceAction, getUserDataAction],
     evaluators: [],
-    services: [new SwapListenerService()],
+    services: [],
     providers: [new AddressProvider()],
 };
